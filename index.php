085fada3fe6990e3dbe6d89b83ec48242eb7b3a4
Anamaria <anarigu_170@hotmail.com>
2023-09-29 10:54:32 -0400

    Creation formulaire resultat

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+?>
+<form method="post" action="formulaire.php">
+ <h1>Formulaire</h1>
+ <label>Nom: </label>
+ <input type="text" name="nom" id="test" size="30" maxlength="20"></br>
+ <label>Prenom: </label>
+ <input type="text" name="prenom" id="test" size="30" maxlength="20"></br>
+<form>    
